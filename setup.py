a90d9f2e1d9380032ec70b4a418209c1e27c8b11
jeffreysiergiej <jjsiergiej81@icloud.com>
2025-05-14 15:46:12 -0500

    Update setup.py
    setup.py to match the structure

--- setup.py
+++ setup.py
@@ -7,7 +7,15 @@ setup(
     author="Jeffrey Siergiej",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    include_package_data=True,
-    install_requires=[],
-    python_requires=">=3.11,<3.12"
+    python_requires=">=3.11,<3.12",
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "pandas",
+        "notebook",
+        "ipython",
+        "pytest",
+        "bandit",
+    ],
 )
