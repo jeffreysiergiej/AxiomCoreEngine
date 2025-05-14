5ebc3dc65e4a87d90b920750d497440beb30e889
jeffreysiergiej <jjsiergiej81@icloud.com>
2025-05-14 16:14:44 -0500

    Update main.py
    strategic foresight

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()  # Load variables from .env
+
+import os
+print("PYTHONPATH is set to:", os.getenv("PYTHONPATH"))
+
 from genesis_ai import genesis_daemon
 from rlq_memory_model import RLQMemoryModel
 from auto_commit_logger import start_commit_loop
