8537cd38bb89463d69f626194f1399f804eb6a65
jeffreysiergiej <jjsiergiej81@icloud.com>
2025-05-14 12:00:09 -0500

    Delete tests/src/test_qpu_kernel.py

--- tests/src/test_qpu_kernel.py
+++
@@ -1,7 +0,0 @@
-from src.axiomcore.qpu_kernel import quantum_superposition
-import numpy as np
-
-def test_quantum_superposition():
-    result = quantum_superposition()
-    expected = np.array([1, 1]) / np.sqrt(2)
-    assert np.allclose(result, expected)
